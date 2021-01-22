7497896d5835925931b48bd7c8e49f1c70956ac7
aman007gupta <aman9453122969@gmail.com>
2021-01-22 16:51:49 +0530

    [Aman]Added: code to lunch site in new tab on clicking logo

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import './App.css';
 import React from 'react'
 
 class App extends React.Component{
-  
+  url = 'https://www.bridgelabz.com/'
   constructor() {
     super()
     this.state = {
       tittle: 'hello from Aman'
     }
   }
+
+  clickOnBLPage = (e) => {
+    console.log("Save Button is clicked", e);
+    window.open(this.url, "_blank")
+  }
   render(){
     return(
       <div class ="App">
         <h1>{this.state.tittle}</h1>
-        <img src={logo}
+        <img src={logo} onClick={this.clickOnBLPage}
         alt= 'BridgeLabz Logo'/>
       </div>
     )

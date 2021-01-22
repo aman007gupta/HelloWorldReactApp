f913427f0d0b9740be2e1cd53cc1cfbb819fa21f
aman007gupta <aman9453122969@gmail.com>
2021-01-22 16:18:09 +0530

    [Aman]Added: code to create react app to display msg

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from 'logo.svg';
+import './App.css';
+import React from 'react'
+import {render} from 'react-dom'
+import {Component} from 'react'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div class ="App">
+        <h1>Hello From Aman</h1>
+      </div>
+    )
+  }
+}
+export default App;

2c90a9e05e64423cf27fbe9eadf50e51fe1495af
aman007gupta <aman9453122969@gmail.com>
2021-01-22 18:01:47 +0530

    [Aman]Added: code to complete hello from bridgelabz page with complete text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from '../src/assets/images/Logo.svg';
+import logo from '../src/assets/images/dd.jpg';
 import './App.css';
 import React from 'react'
 
@@ -15,6 +15,7 @@ class App extends React.Component{
     console.log("Save Button is clicked", e);
     window.open(this.url, "_blank")
   }
+
   onNameChange = (event) => {
     const nameRegex = RegExp('^[A-z]{1}[A-Za-z\\s]{2,}$')
     console.log(event.target.value)
@@ -31,6 +32,7 @@ class App extends React.Component{
     })
     }
   }
+  
   render(){
     return(
       <>
@@ -38,10 +40,20 @@ class App extends React.Component{
           <h1>Hello {this.state.name} from Bridgelabz</h1>
           <img src={logo} onClick={this.clickOnBLPage}
           alt= 'BridgeLabz Logo'/>
-        </div>
-        <div>
           <input onChange={this.onNameChange}/>
           <span className="error-output">{this.state.nameError}</span>
+          <p>At BridgeLabz, we're a techie community of</p>
+        <ul>
+            <li>technologists</li>
+            <li>thinkers</li>
+            <li>builders</li>
+        </ul>
+        <p>Working together to keep the tech employability of Engineer alive and assess,
+            so Tech Companies worldwide can get contributors and creators for Technology Solution.
+            We believe this act of human collaboration across an employability platform is essential
+            to individual growth and our collective future.</p>
+        <p>To know about us, visit <a href="https://www.bridgelabz.com/">BridgeLabz</a> to
+           learn even more about our mission i.e. <strong>Employability to all</strong>.</p>
         </div>
       </>
     )
